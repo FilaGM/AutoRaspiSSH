30fcb7b6372614dda3838bf1f3bb2f02047c6c96
Hades_ <72222221+FilaGM@users.noreply.github.com>
2021-05-10 14:08:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-import threading,os,paramiko
+import threading,os,paramiko,time,json,sys
 
-out = open("out.txt","w")
-
+config = json.loads(open("config.json","r").read())
+out = []
 found = False
 
+done = 0
+
+def addOut(data):
+    global out
+    out.append(data)
+    
+    ot = open(config["log_file_name"],"w")
+    for a in out:
+        ot.write(a+"\n")
+    ot.close()
+    lastOut = out
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+    valueScaled = float(value - leftMin) / float(leftSpan)
+    
+    return rightMin + (valueScaled * rightSpan)
+
+def loadingBar(val,pretext,text,loadBarLength):
+    global maxThreads
+    value = translate(val,0,maxThreads,0,loadBarLength)
+    value = int(value)
+    
+    percentage = int(val/(maxThreads/100))
+    
+    sys.stdout.write("\r" + str(pretext) + "[" + "#"*value + "-"*(loadBarLength-value) + "] " + str(percentage) + "% "+ text + str(val))
+    sys.stdout.flush()
+    
 def ping(host):
-    global out,found
+    global out,found,done
     
     ot = os.popen('ping -n 1 ' + host).read()
     
@@ -13,17 +42,44 @@ def ping(host):
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(host, username="pi", password="raspberry")
-            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("hostname -i")
-            os.system("putty.exe "+host+" -l pi -pw raspberry")
-            print(host)
-            out.write(str(host))
-            exit()
+            
+            if config["ssh"]["password"] != "":
+                ssh.connect(host, username=config["ssh"]["username"], password=config["ssh"]["password"])
+            else:
+                ssh.connect(host, username=config["ssh"]["username"])
+                
+            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("hostname -I")
+            
+            if config["autoSSH"]:
+                os.system("putty.exe "+host+" -l pi -pw raspberry")
+                
+            addOut("Successfuly connected to: " + host)
         except Exception as err:
-            print(err)
-        
-for a in range(254):
+            addOut("Error at host: " + host + " => " + str(err))
+            pass
+    
+    done += 1
+    
+maxThreads = 254
+loadBarLength = 20
+
+sys.stdout.write("Starting threads:\n")
+
+for a in range(maxThreads):
+    loadingBar(a+1," "," Active threads: ",20)
+    
     thread = threading.Thread(target=ping,args=(("192.168.1."+str(a+1)),))
     thread.start()
+
+sys.stdout.write("\nProgress: \n")
+
+while True:
     
-input()
+    loadingBar(done," "," Done threads: ",20)
+    
+    if done == maxThreads:
+        loadingBar(done," "," Done threads: ",20)
+        break
+    
+    time.sleep(0.1)
+   
